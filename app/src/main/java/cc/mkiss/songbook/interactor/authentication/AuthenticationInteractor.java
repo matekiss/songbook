f3f23b9e8cb19f7203b6006f2ce70c1d3dce04b6
Máté Kiss <kissmate@gmail.com>
2017-05-14 16:28:41 +0200

    Use network in authentication interactor

--- app/src/main/java/cc/mkiss/songbook/interactor/authentication/AuthenticationInteractor.java
+++ app/src/main/java/cc/mkiss/songbook/interactor/authentication/AuthenticationInteractor.java
@@ -2,10 +2,15 @@ package cc.mkiss.songbook.interactor.authentication;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import cc.mkiss.songbook.SongbookApplication;
 import cc.mkiss.songbook.interactor.authentication.events.LoginEvent;
+import cc.mkiss.songbook.interactor.authentication.events.LogoutEvent;
+import cc.mkiss.songbook.model.Credentials;
+import cc.mkiss.songbook.network.api.UserApi;
 import cc.mkiss.songbook.repository.Repository;
 
 public class AuthenticationInteractor {
@@ -14,12 +19,35 @@ public class AuthenticationInteractor {
     @Inject
     EventBus eventBus;
 
+    @Inject
+    UserApi userApi;
+
     public AuthenticationInteractor() {
         SongbookApplication.injector.inject(this);
     }
 
     public void login(String username, String password) {
         LoginEvent event = new LoginEvent();
+
+        Credentials credentials = new Credentials(username, password);
+        try {
+            event.setToken(userApi.loginUser(credentials).execute().body());
+        } catch (Exception e) {
+            event.setThrowable(e);
+        }
+
+        eventBus.post(event);
+    }
+
+    public void logout() {
+        LogoutEvent event = new LogoutEvent();
+
+        try {
+            userApi.logoutUser().execute();
+        } catch (Exception e) {
+            event.setThrowable(e);
+        }
+
         eventBus.post(event);
     }
 }
--- app/src/main/java/cc/mkiss/songbook/interactor/authentication/events/LoginEvent.java
+++ app/src/main/java/cc/mkiss/songbook/interactor/authentication/events/LoginEvent.java
@@ -3,4 +3,13 @@ package cc.mkiss.songbook.interactor.authentication.events;
 import cc.mkiss.songbook.interactor.Event;
 
 public class LoginEvent extends Event {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
