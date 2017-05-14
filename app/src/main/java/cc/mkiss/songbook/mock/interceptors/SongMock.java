3d66286e6f3f851a93218f722e417d2f9adaa4a7
Máté Kiss <kissmate@gmail.com>
2017-05-14 15:18:09 +0200

    Implement song network mock

--- app/src/main/java/cc/mkiss/songbook/mock/interceptors/SongMock.java
+++ app/src/main/java/cc/mkiss/songbook/mock/interceptors/SongMock.java
@@ -3,6 +3,9 @@ package cc.mkiss.songbook.mock.interceptors;
 import android.net.Uri;
 
 import cc.mkiss.songbook.network.NetworkConfig;
+import cc.mkiss.songbook.repository.MemoryRepository;
+import cc.mkiss.songbook.repository.Repository;
+import cc.mkiss.songbook.utils.GsonHelper;
 import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -17,8 +20,34 @@ public class SongMock {
         int responseCode;
         Headers headers = request.headers();
 
-        responseString = "";
-        responseCode = 200;
+        if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "song")
+                && request.method().equals("GET")) {
+            Repository repository = new MemoryRepository();
+            repository.open(null);
+            responseString = GsonHelper.getGson().toJson(repository.getSongs(""));
+            responseCode = 200;
+        } else if (uri.getPath().equals(NetworkConfig.ENDPOINT_PREFIX + "song")
+                && request.method().equals("POST")) {
+            responseString = "";
+            responseCode = 200;
+        } else if (uri.getPath().startsWith(NetworkConfig.ENDPOINT_PREFIX + "song")
+                && request.method().equals("GET")) {
+            Repository repository = new MemoryRepository();
+            repository.open(null);
+            responseString = GsonHelper.getGson().toJson(repository.getSongs("").get(0));
+            responseCode = 200;
+        } else if (uri.getPath().startsWith(NetworkConfig.ENDPOINT_PREFIX + "song")
+                && request.method().equals("PUT")) {
+            responseString = "";
+            responseCode = 200;
+        } else if (uri.getPath().startsWith(NetworkConfig.ENDPOINT_PREFIX + "song")
+                && request.method().equals("DELETE")) {
+            responseString = "";
+            responseCode = 200;
+        } else {
+            responseString = "";
+            responseCode = 200;
+        }
 
         return makeResponse(request, headers, responseCode, responseString);
     }
